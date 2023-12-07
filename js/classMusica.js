7ec45d7b66794ba0ac16bd65b684eeca075838be
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-12-07 15:22:54 -0300

    se creó la clase música, se añadió el constructor

---
+++ js/classMusica.js
@@ -0,0 +1,19 @@
+export default class Musica {
+    #id;
+    #titulo;
+    #artista;
+    #categoria;
+    #imagen;
+    #duracion;
+
+    constructor(id, titulo, artista, categoria, imagen, duracion) {
+        this.#id = id;
+        this.#titulo = titulo;
+        this.#artista = artista;
+        this.#categoria = categoria;
+        this.#imagen = imagen;
+        this.#duracion = duracion;
+    }
+
+
+}
