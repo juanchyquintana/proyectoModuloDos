adccf491cbfd5720bc28d86909a97c3ca76322b2
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-12-07 15:25:13 -0300

    se agregaron los metodos (get y set)

--- js/classMusica.js
+++ js/classMusica.js
@@ -15,5 +15,47 @@ export default class Musica {
         this.#duracion = duracion;
     }
 
+    get id() {
+        return this.#id;
+    }
 
+    get titulo() {
+        return this.#titulo
+    }
+
+    set titulo(nuevoTitulo) {
+        this.#titulo = nuevoTitulo;
+    }
+
+    get artista() {
+        return this.#artista;
+    }
+
+    set artista(nuevoArtista) {
+        this.#artista = nuevoArtista
+    }
+
+    get categoria() {
+        return this.#categoria;
+    }
+
+    set categoria(nuevaCategoria) {
+        this.#categoria = nuevaCategoria;
+    }
+
+    get imagen() {
+        return this.#imagen;
+    }
+
+    set imagen(nuevaImagen) {
+        this.#imagen = nuevaImagen;
+    }
+
+    get duracion() {
+        return this.#duracion;
+    }
+
+    set duracion(nuevaDuracion) {
+        this.#duracion = nuevaDuracion
+    }
 }
