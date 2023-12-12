e184296df0a3bcf232becd9863b59bed2d735145
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-12-12 11:21:35 -0300

    se creó la carpeta crud para iniciar con la funcionalidades. iniciando por la de create

---
+++ js/crud.js
@@ -0,0 +1,9 @@
+import Musica from "./classMusica.js";
+
+const musica = JSON.parse(localStorage.getItem("musicaKey")) || [];
+const musicaPrueba = []
+
+const crearContacto = () => {}
+const guardarMusicaLocalStorage = () => {}
+const limparFormularioMusicaAdmin = () => {}
+const cargaInicial = () => {}
