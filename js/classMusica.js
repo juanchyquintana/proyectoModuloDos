b33191580265ce351883fcd7edc35d40cc8f0d86
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-12-12 11:05:45 -0300

    se crearon los metodos de la clase musica. Los get y set de las propiedades

--- js/classMusica.js
+++ js/classMusica.js
@@ -1,17 +1,61 @@
 export default class Musica {
-    #id;
-    #titulo;
-    #artista;
-    #categoria;
-    #imagen;
-    #duracion;
+  #id;
+  #titulo;
+  #artista;
+  #categoria;
+  #imagen;
+  #duracion;
 
-    constructor(id, titulo, artista, categoria, imagen, duracion) {
-        this.#id = id;
-        this.#titulo = titulo;
-        this.#artista = artista;
-        this.#categoria = categoria;
-        this.#imagen = imagen;
-        this.#duracion = duracion;
-    }
+  constructor(id, titulo, artista, categoria, imagen, duracion) {
+    this.#id = id;
+    this.#titulo = titulo;
+    this.#artista = artista;
+    this.#categoria = categoria;
+    this.#imagen = imagen;
+    this.#duracion = duracion;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get titulo() {
+    return this.#titulo;
+  }
+
+  set titulo(nuevoTitulo) {
+    this.#titulo = nuevoTitulo;
+  }
+
+  get artista() {
+    return this.#artista;
+  }
+
+  set artista(nuevoArtista) {
+    this.#artista = nuevoArtista;
+  }
+
+  get categoria() {
+    return this.#categoria;
+  }
+
+  set categoria(nuevaCategoria) {
+    this.#categoria = nuevaCategoria;
+  }
+
+  get imagen() {
+    return this.#imagen;
+  }
+
+  set imagen(nuevaImagen) {
+    this.#imagen = nuevaImagen;
+  }
+
+  get duracion() {
+    return this.#duracion;
+  }
+
+  set duracion(nuevaDuracion) {
+    this.#duracion = nuevaDuracion;
+  }
 }
