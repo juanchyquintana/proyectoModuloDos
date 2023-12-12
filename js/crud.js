63c218677e091c4c98860789af5e92c75c7e0e6f
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-12-12 11:33:00 -0300

    se crearon las funciones de crearMusica, guardar en local storage, limpiar formulario  y se agregó la funcion de crear campo

--- js/crud.js
+++ js/crud.js
@@ -1,9 +1,55 @@
 import Musica from "./classMusica.js";
 
 const musica = JSON.parse(localStorage.getItem("musicaKey")) || [];
-const musicaPrueba = []
+const formularioAgregarMusica = document.querySelector("#");
+const nombreCancion = document.querySelector("#");
+const artista = document.querySelector("#autor");
+const categoria = document.querySelector("#categoria");
+const imagen = document.querySelector("#");
+const duracion = document.querySelector("#");
 
-const crearContacto = () => {}
-const guardarMusicaLocalStorage = () => {}
-const limparFormularioMusicaAdmin = () => {}
-const cargaInicial = () => {}
+const crearMusica = (e) => {
+  e.preventDefault();
+
+  const musicaNueva = new Musica(
+    crypto.randomUUID(),
+    nombreCancion.value,
+    artista.value,
+    categoria.value,
+    imagen.value,
+    duracion.value
+  );
+
+  if (
+    [
+      nombreCancion.value,
+      artista.value,
+      categoria.value,
+      imagen.value,
+      duracion.value,
+    ].includes("")
+  ) {
+    alert("Todos los campos son obligatorios");
+  } else {
+    musica.push(musicaNueva);
+
+    guardarMusicaLocalStorage();
+
+    limparFormularioMusicaAdmin();
+
+    crearCampoNuevo(musicaNueva, musica.length)
+  }
+};
+
+const guardarMusicaLocalStorage = () => {
+  localStorage.setItem("musicaKey", JSON.stringify(musica));
+};
+
+const limparFormularioMusicaAdmin = () => {
+  formularioAgregarMusica.reset();
+};
+
+const crearCampoNuevo = () => {}
+const cargaInicial = () => {};
+
+formularioAgregarMusica.addEventListener("submit", crearMusica);
