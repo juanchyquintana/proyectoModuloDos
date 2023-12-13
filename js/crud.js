18878c414f44826d633373c013e2178b58494b7c
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-12-13 19:47:54 -0300

    se quitó una parte que no me tocaba hacer

--- js/crud.js
+++ js/crud.js
@@ -1,4 +1,4 @@
-import Musica from './classMusica.js';
+import Musica from "./classMusica.js";
 
 const musica = JSON.parse(localStorage.getItem("musicaKey")) || [];
 const formularioAgregarMusica = document.querySelector("#formularioMusica");
@@ -7,7 +7,8 @@ const artista = document.querySelector("#autor");
 const categoria = document.querySelector("#categoria");
 const imagen = document.querySelector("#musica");
 const duracion = document.querySelector("#duracion");
-let patronDuracionCancion = /^(?:(?:(?:(?:[0-5]?[0-9]):)?[0-5]?[0-9])|(?:(?:[0-5]?[0-9])s))$/;
+let patronDuracionCancion =
+  /^(?:(?:(?:(?:[0-5]?[0-9]):)?[0-5]?[0-9])|(?:(?:[0-5]?[0-9])s))$/;
 
 const crearMusica = (e) => {
   e.preventDefault();
@@ -21,9 +22,9 @@ const crearMusica = (e) => {
     duracion.value
   );
 
-  if(!patronDuracionCancion.test(duracion.value)) {
-    alert('no es un numero valido')
-    return
+  if (!patronDuracionCancion.test(duracion.value)) {
+    alert("no es un numero valido");
+    return;
   }
 
   if (
@@ -32,7 +33,7 @@ const crearMusica = (e) => {
       artista.value,
       categoria.value,
       imagen.value,
-      duracion.value
+      duracion.value,
     ].includes("")
   ) {
     Swal.fire({
@@ -40,7 +41,7 @@ const crearMusica = (e) => {
       icon: "error",
       title: "Campos Vacios",
       showConfirmButton: false,
-      timer: 1500
+      timer: 1500,
     });
   } else {
     musica.push(musicaNueva);
@@ -48,8 +49,6 @@ const crearMusica = (e) => {
     guardarMusicaLocalStorage();
 
     limparFormularioMusicaAdmin();
-
-    crearCampoNuevo(musicaNueva, musica.length);
 
     const Toast = Swal.mixin({
       toast: true,
@@ -60,13 +59,12 @@ const crearMusica = (e) => {
       didOpen: (toast) => {
         toast.onmouseenter = Swal.stopTimer;
         toast.onmouseleave = Swal.resumeTimer;
-      }
+      },
     });
     Toast.fire({
       icon: "success",
-      title: "Agregado Correctamente"
+      title: "Agregado Correctamente",
     });
-
   }
 };
 
@@ -78,46 +76,4 @@ const limparFormularioMusicaAdmin = () => {
   formularioAgregarMusica.reset();
 };
 
-const crearCampoNuevo = (musica, fila) => {
-  const tablaMusica = document.querySelector("#tablaMusica");
-  tablaMusica.innerHTML += `
-    <tr>
-        <td>${fila}</td>
-        <td>${musica.titulo}</td>
-        <td>${musica.artista}</td>
-        <td>${musica.duracion}</td>
-    <td class="d-sm-flex">
-      <a 
-        type="button" 
-        class="boton m-2" 
-        id="boton-editar" 
-        href="./adminU.html"
-        >
-            <span class="bn39span">Editar</span>
-      </a>
-
-      <button type="button" class="boton m-2" id="boton-borrar">
-        <span class="bn39span">Borrar</span>
-      </button>
-
-      <a 
-        type="button" 
-        class="boton m-2" 
-        id="boton-verMas" 
-        href="#"
-        >
-            <span class="bn39span">Ver mas</span>
-      </a>
-    </td>
-  </tr>
-    `;
-};
-
-const cargaInicial = () => {
-    if(musica.length > 0) {
-        musica.map((musica, posicion) => crearCampoNuevo(musica, posicion + 1))
-    }
-};
-
 formularioAgregarMusica.addEventListener("submit", crearMusica);
-cargaInicial();
