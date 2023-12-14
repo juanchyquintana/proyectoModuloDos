c0647dbb276e02ea65584cf07db4b8cd1bbd73d3
Fiol Thiago <fiolthiago9@gmail.com>
2023-12-14 17:30:29 -0300

    me quede atascado en el form

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,4 +1,3 @@
-import Swal from "sweetalert2";
 const form = document.querySelector("form"),
   nombre = document.getElementById("nombre"),
   autor = document.getElementById("autor"),
@@ -7,7 +6,7 @@ const form = document.querySelector("form"),
 
 function validarCaracteres(input, max, min) {
   const valor = input.value.trim();
-  return valor.length() >= min && valor.length() <= max && valor !== "";
+  return valor.length >= min && valor.length <= max && valor !== "";
 }
 function validarCategoria() {
   const generos = [
@@ -27,16 +26,54 @@ function validarCategoria() {
       break;
     }
   }
-  return false;
+  return verificacion;
 }
-function validarMusica() {
-  
-}
+
 function validacionTotal(e) {
   e.preventDefault();
   const nombreBool = validarCaracteres(nombre, 100, 3),
     autorBool = validarCaracteres(autor, 70, 3),
     categoriaBool = validarCategoria();
+  let tituloTexto = "",
+    autorTexto = "",
+    categoriaTexto = "",
+    musicaTexto = "";
+  if (nombreBool === false) {
+    tituloTexto = "<p>El titulo esta incorrecto, vuelve a intentarlo</p>";
+  }
+  if (autorBool === false) {
+    autorTexto = "<p>El autor esta incorrecto, vuelve a intentarlo</p>";
+  }
+  if (categoriaBool === false) {
+    categoriaTexto =
+      "<p>La categoria no esta en el rango que dimos vuelve a intentarlo</p>";
+  }
+  if (musica.value === "") {
+    musicaTexto = "<p>No hay musica</p>";
+  }
+  if (nombreBool && autorBool && categoriaBool && musica.value !== "") {
+    return true;
+  } else {
+    Swal.fire({
+      title: "<strong>HTML <u>example</u></strong>",
+      icon: "info",
+      html: `
+        You can use <b>bold text</b>,
+        <a href="#">links</a>,
+        and other HTML tags
+      `,
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: `
+        <i class="fa fa-thumbs-up"></i> Great!
+      `,
+      confirmButtonAriaLabel: "Thumbs up, great!",
+      cancelButtonText: `
+        <i class="fa fa-thumbs-down"></i>
+      `,
+      cancelButtonAriaLabel: "Thumbs down",
+    });
+  }
 }
-
 form.addEventListener("submit", validacionTotal);
