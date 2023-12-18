31c7996961d0b8228599a7b1da7463ecf7cbf734
Fiol Thiago <fiolthiago9@gmail.com>
2023-12-18 00:05:52 -0300

    agregamos las primeras variables y un bucle para verificar las caciones buscadas

--- js/filtroLogica.js
+++ js/filtroLogica.js
@@ -1,2 +1,12 @@
 const param = new URLSearchParams(window.location.search);
 const valor = param.get("value");
+const musicas = JSON.parse(localStorage.getItem("musicaKey")) || [];
+const musicasEncontradas = []
+let encontro = false;
+
+for(let i = 0; i < musicas.length; i++){
+    if(valor === musicas[i].titulo || valor === musicas[i].artista && valor === musicas[i].categoria){
+        encontro = true;
+        musicasEncontradas.push(musicas[i].id)
+    }
+}
